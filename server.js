a51f3aa81d3b4592fa847e4a981f9a815e652998
dataworx <data_worx@live.com>
2015-09-15 11:23:02 -0500

    remove app.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 var restify = require('restify');
 
 var options = {
-    name: 'oauth2-token-server'
+    name: 'oauth2-token-server',
+    version: '0.1.0'
 };
 
 var server = restify.createServer(options);
